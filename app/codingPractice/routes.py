5741b67bd11040e7253ad4bec32ddc400ca38c63
anaungurean <100145653+anaungurean@users.noreply.github.com>
2024-04-16 19:42:20 +0300

    -add-api

--- app/codingPractice/routes.py
+++ app/codingPractice/routes.py
@@ -8,11 +8,19 @@ def get_all_problems():
     problems = Problem.get_all_problems()
     return jsonify(problems), 200
 
-@problem_bp.route('/problems/<int:problem_id>', methods=['POST'])
+@problem_bp.route('/problems/<int:problem_id>', methods=['GET'])
 def get_problem_by_id(problem_id):
     problem = Problem.get_problem_by_id(problem_id)
     if problem:
         return jsonify(problem), 200
+    else:
+        return jsonify({"error": "Problem not found"}), 404
+
+@problem_bp.route('/problems/<string:title>', methods=['GET'])
+def get_id_by_title(title):
+    problem = Problem.query.filter_by(title=title).first()
+    if problem:
+        return jsonify(problem.id), 200
     else:
         return jsonify({"error": "Problem not found"}), 404
 
