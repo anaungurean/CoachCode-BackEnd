0a21725e0749b0f470597034f38303ddf19917ce
anaungurean <100145653+anaungurean@users.noreply.github.com>
2024-06-08 23:06:49 +0300

    -add-mute

--- app/chatBot/routes.py
+++ app/chatBot/routes.py
@@ -12,16 +12,16 @@ def chat():
     user_message = data.get("message")
     print(user_message)
 
-    response = openai.ChatCompletion.create(
-        model='gpt-4',
-        messages=[
-            {"role": "system",
-             "content": 'You are a highly skilled AI, answer the questions given within a maximum of 100 characters.'},
-            {"role": "user", "content": user_message}
-        ]
-    )
+    # response = openai.ChatCompletion.create(
+    #     model='gpt-4',
+    #     messages=[
+    #         {"role": "system",
+    #          "content": 'You are a highly skilled AI, answer the questions given within a maximum of 100 characters.'},
+    #         {"role": "user", "content": user_message}
+    #     ]
+    # )
 
-    chat_response = response.choices[0].message['content']
-    # chat_response = 'Hello I am a chatbot. I am here to help you.'
-    # print(chat_response)
+    # chat_response = response.choices[0].message['content']
+    chat_response = 'Hello I am a chatbot. I am here to help you.'
+    print(chat_response)
     return jsonify(chat_response)
